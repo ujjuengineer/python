94422d27d712b27c8df6bfb122a89a28184199ea
ujjuengineer <uk3945351@gmail.com>
2024-11-18 11:05:26 +0530

    added files

---
+++ chapter_4/1.dictionary.py
@@ -0,0 +1,7 @@
+"""
+Dictionaries are used to store data value in key:value pair , (just like class in cpp)
+they are unordered, mutable and don't allow duplicate keys.
+
+"""
+
+# keys can be string, number but it can't be list or tupple, but the values can be
